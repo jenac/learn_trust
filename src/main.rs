c6de5a12d870a6965df74e21cfe98015cb04de41
Lihe Chen <lihe.chen@gmail.com>
2021-05-18 20:54:27 -0500

    2-2
    Signed-off-by: Lihe Chen <lihe.chen@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::io;
 
 fn main() {
+    e2_2();
+    println!("----------");
+    e2_1();
+    
+}
+
+fn e2_1() {
     println!("Guess the number!");
     println!("Please input your guess.");
     
@@ -8,4 +15,10 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("Failed to read line");
     
     println!("You guessed: {}", guess);
+}
+
+fn e2_2() {
+    let x = 5;
+    let y = 6;
+    println!("x={}, y={}", x, y)
 }
