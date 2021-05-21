55be4ab10ca651092783a57fa9274bb1d7888660
Lihe Chen <lihe.chen@gmail.com>
2021-05-20 20:44:39 -0500

    2-6
    Signed-off-by: Lihe Chen <lihe.chen@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,25 @@ fn main() {
 fn e2_1() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
+    // println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
-    
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please give a number!");
-    
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => { println!("need a number"); continue },
+        };
+        
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less =>  println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!")
+        match guess.cmp(&secret_number) {
+            Ordering::Less =>  println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => { println!("You win!"); break; }
+        }
     }
 }
 
